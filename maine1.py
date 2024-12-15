4c10c207d80640f81e540025cf47740b8e1b983e
Alenatem <151952506+Alenatem@users.noreply.github.com>
2024-12-15 18:27:47 +0400

    Add files via upload

---
+++ maine1.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+driver.get('https://www.lambdatest.com/selenium-playground/simple-form-demo')
+
+user_message = driver.find_element(By.ID, "user-message")
+user_message.send_keys("name")
+sleep(2)
